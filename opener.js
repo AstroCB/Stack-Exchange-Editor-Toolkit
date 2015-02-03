f83e4adb5d67ae7679ec33397ecd2abf75ba7e01
Cameron Bernhardt <cambernhardt@me.com>
2015-02-02 22:53:08 -0500

    Changed behavior of AJAX rule (fixed #45)

--- opener.js
+++ opener.js
@@ -4,7 +4,7 @@ function editPage(url){
   win.addEventListener("load", function(){
     document.getElementsByClassName("edit-post")[0].click();
     win.setTimeout(function(){
-      document.getElementById("fix").click();
+      document.getElementById("ToolkitFix").click();
     }, 1000);
     win.setTimeout(function(){
       document.getElementsByTagName("input")[14].click();
